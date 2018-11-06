e6c96d9330737b5ef67a0083e930c791f7667ede
pouyarahmti <39616083+pouyarahmti@users.noreply.github.com>
2018-11-06 10:19:10 +0330

    compeleting the  AnyFoodSearchProblem Compeleting the FindPathtoClosestDot

--- searchAgents.py
+++ searchAgents.py
@@ -485,6 +485,7 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
+        return search.aStarSearch(problem)
         util.raiseNotDefined()
 
 class AnyFoodSearchProblem(PositionSearchProblem):
@@ -521,7 +522,7 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         x,y = state
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.food[x][y]
 
 def mazeDistance(point1, point2, gameState):
     """
